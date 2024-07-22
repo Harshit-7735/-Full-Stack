8fb07e395629cefb12724d284f4fc81697fb7e34
Harshit-7735 <letcs202205@global.org.in>
2024-07-22 10:23:14 +0530

    some objects

---
+++ Javascript/index.js
@@ -0,0 +1,19 @@
+console.log('hi');
+console.log(console);
+console.warn('warning');
+console.error('error');
+console.table({a: 1, b: 2});
+console.log(window);
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+console.log(window.location);
+
+window.alert('hello');
+// window.prompt('enter something');
+// window.confirm('are you sure?');
+const name=window.prompt('enter your name');
+console.log(name);
+
+
+const names=['john', 'peter', 'bob'];
+console.log(names);
