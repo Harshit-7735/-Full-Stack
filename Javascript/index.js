9f54b56059f6b85fc73cda6f379f70bce79eabc3
Harshit-7735 <letcs202205@global.org.in>
2024-07-22 11:01:12 +0530

    Objects in javascript

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,19 +1,37 @@
-console.log('hi');
-console.log(console);
-console.warn('warning');
-console.error('error');
-console.table({a: 1, b: 2});
-console.log(window);
-console.log(window.innerHeight);
-console.log(window.innerWidth);
-console.log(window.location);
+// JSON :java script object notation
+// JSON is a syntax for storing and exchanging data.
 
-window.alert('hello');
-// window.prompt('enter something');
-// window.confirm('are you sure?');
-const name=window.prompt('enter your name');
-console.log(name);
+const obj1 = {
+  name: "Harshit",
+  age: 22,
+  ismarried: true,
+};
 
-
-const names=['john', 'peter', 'bob'];
-console.log(names);
+const car={
+    model:"Audi",
+    year:2021,
+}
+const obj = {
+  name: "Harshit",
+  age: 22,
+  ismarried: false,
+  // function inside object is called method
+  greet: function () {
+    console.log("Hello there");
+  },
+  // an object can have another object as a property
+  address: {
+    city: "Delhi",
+    state: "Delhi",
+  },
+  //an object can have array inside an object as a property
+  hobbies: ["coding", "reading", "travelling"],
+  //an object can have another object as a property
+  obj1: obj1,
+  car:car,
+};
+console.log(obj);
+// accessing object properties
+obj.greet();
+console.log(obj.address);
+console.log(obj.hobbies);
