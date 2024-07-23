65048fcc5521cc2a780dbc073fe29e158c0380a2
Harshit-7735 <letcs202205@global.org.in>
2024-07-23 11:03:57 +0530

    Factory Funstions

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,45 +1,7 @@
-/*
-class car {
-  name = "BMW"; // property of class car
-  // consructor is a special method in class which is called when an object is created of that class
-  constructor() {
-    console.log("Constructor called");
-  }
-}
-// Creating an object of class car
-// new keyword is used to create an object of class
-const car1 = new car();
-console.log(car1);
-*/
+// Factory function for creating a new object that represents a single task item in the list of tasks.
 
-/*
-class car {
-  constructor(name, brand) {
-    this.name = name;
-    this.brand = brand;
-    // this is pointing to the object of the class car which is created using new keyword
-    console.log(this); // this keyword is used to access the properties of the class inside the class itself
-  }
+function bike(){
+  console.log(this); // points to the global object
 }
-const car1 = new car("BMW", "X5");
-// console.log(car1);
-*/
-
-class car {
-  // we can also define properties of class outside the constructor method
-  age = 30;
-  adult = true;
-  constructor(name, brand) {
-    this.name = name;
-    this.brand = brand;
-    this.age = 40; // we can also change the value of properties of class inside the constructor method using this keyword
-
-    console.log(this); 
-  }
-  // we can also define methods of class outside the constructor method using function keyword
-  greet() {
-    console.log("Hello, I am " + this.name);
-  }
-}
-const car1 = new car("BMW", "X5");
-car1.greet(); // calling the method of class car using object of class car
+const bike1 = new bike(); // not pointing to the global object
+console.log(bike1);
