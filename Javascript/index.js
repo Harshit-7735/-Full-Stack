794a4da20b9d2f9702f2ac512bd874a214350893
Harshit-7735 <letcs202205@global.org.in>
2024-07-23 11:29:08 +0530

    pass by value and pass by reference

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,19 @@
-// Factory function for creating a new object that represents a single task item in the list of tasks.
-
-function bike(){
-  console.log(this); // points to the global object
+// pass by reference
+const obj={
+  name:"john",
+  // age:30,
 }
-const bike1 = new bike(); // not pointing to the global object
-console.log(bike1);
+// javascript is a reference type language so when we create a object and assign it to another object then both objects will point to the same memory location
+const obj1=obj;
+obj.name="peter"; // we can override the value of original object by changing the value of obj1
+obj1.age=25; // we can add new property to the original object by adding the property to obj1
+console.log(obj1);
+
+
+// primitive types are not reference type so when we assign a value to another variable then both variables will point to different memory location
+//pass by value
+let a=10;
+let b=a;
+a=20;
+console.log(b); // it will print 10
+console.log(a); // it will print 20
