b5243e68fe17f1151006d8841a9b9b669cfd54b4
Harshit-7735 <letcs202205@global.org.in>
2024-07-24 13:40:10 +0530

    Arrow Functions

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,104 +1,70 @@
-const a = {
-  name: "Harshit",
-  age: 30,
-  isMarried: true,
+// const greet=function(){
+//     console.log('Hello');
+// }
+// greet();
 
+//                  BY using arrowFunction
+
+// const greet=()=>{
+//     console.log('Hello');
+// }
+// greet();
+
+// const greet=()=>console.log('Hello');
+// greet();
+
+// Wap to add two numbers
+/*
+const sum=function(a,b){
+  return a+b;
+}
+console.log(sum(10,20));
+*/
+/*
+const Arrowsum=(a,b)=>a+b;
+console.log(Arrowsum(10,20));*/
+
+/*
+const arr=[{name:'Rahul',age:20},{name:'Rohit',age:21},{name:'Raj',age:22}];
+const newArr=arr.filter(item=>item.age>20);
+console.log(newArr);
+*/
+
+/*
+const a = {
+  name: "Rahul",
+  age: 20,
   greet: function () {
     console.log("Hello");
   },
 };
-// for (const key in a) {
-//   console.log(key, a[key]);
-// }
 
-// console.log(Object.keys(a));
-// console.log(Object.values(a));
-
-// const b={}
-// const reducefunction =function(prev,curr){
-//   console.log(prev,curr);
-//   // Dry run of the code below
-//   // prev={}
-//   // curr=name
-//   // prev[name]=a[name]
-//   // prev={name:"Harshit"}
-//   // curr=age
-//   // prev[age]=a[age]
-//   // prev={name:"Harshit",age:30}
-//   // curr=isMarried
-//   // prev[isMarried]=a[isMarried]
-//   // prev={name:"Harshit",age:30,isMarried:true}
-//   // curr=greet
-//   // prev[greet]=a[greet]
-//   // prev={name:"Harshit",age:30,isMarried:true,greet:function(){console.log("Hello")}}
-//   prev[curr]=a[curr]
-//   return prev
-// }
-// Object.keys(a).reduce(reducefunction,b)
-// console.log(b);
-
-const arr = [
-  { name: "Harshit", age: 30 },
-  { name: "Amit", age: 25 },
-  { name: "Rahul", age: 35 },
-];
-/*
-const b={}
-const reducefunction=function(prev,curr){
-  // dry run of the code below
-  // prev={}
-  // curr={name:"Harshit",age:30}
-  // prev[name]=age
-  // prev={Harshit:30}
-  // curr={name:"Amit",age:25}
-  // prev[Amit]=25
-  // prev={Harshit:30,Amit:25}
-  // curr={name:"Rahul",age:35}
-  // prev[Rahul]=35
-  // prev={Harshit:30,Amit:25,Rahul:35}
-  prev[curr.name]=curr.age // it will return the object with name as key and age as value 
-  return prev
-}
-console.log(arr.reduce(reducefunction,b));  
+const b = Object.keys(a).reduce((prev, curr) => {
+  console.log(prev, curr);
+  prev[curr] = a[curr];
+  return prev;
+}, {});
 */
 
-/*
-const findFn = function (item, index) {
-  if (item.name === "Amit") {
-    return true;
-  }
-  return false;
+// const arr = [1, 2, 3, 4, 5];
+// const sum=arr.reduce((prev,curr)=>prev+curr,0);
+// console.log(sum);
+
+const obj = {
+  name: "Rahul",
+  age: 20,
+  greet: function () { // Normal function has its own this keyword so it will take the this keyword of its parent
+    console.log(this);
+  },
+  greetArrow: () => { // Arrow function does not have its own this keyword so it will take the this keyword of its parent
+    console.log(this);
+  },
 };
-const ele = arr.find(findFn);
-console.log(ele); // {name:"Amit",age:25}
-*/
+obj.greet();
+obj.greetArrow();
 
-/*
-const filterfn=function(item,index){
-  if(item.age>30 && item.name==="Rahul"){
-    return true
-  }
-  return false
+
+const greet = function (Name,age) {
+  console.log("Hello " + Name + " Your age is " + age);
 }
-const ele=arr.filter(filterfn)
-console.log(ele); // [{name:"Rahul",age:35}]
-*/
-
-/*
-const arrr=[1,2,3,4,5]
-const reduceFn=function(prev,curr){
-  return prev+curr
-}
-console.log(arrr.reduce(reduceFn,0)); // 15
-*/
-
-const arrr = [1, 2, 3, 4, 5];
-arrr.splice(1,0,99)
-arrr.splice(2,0,100)
-arrr.splice(3,0,101)
-console.log(arrr);
-
-console.log(arrr.splice(1,5)); // [99,100,101,2,3]
-console.log(arrr.splice(2,1)); // [100]
-console.log(arrr.splice(3,1)); // [101]
-console.log(arrr); // [1,2,3,4,5]
+greet(20);
