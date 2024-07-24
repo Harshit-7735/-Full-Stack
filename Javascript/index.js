9d9e54f0316aeeb8b3570fc1479b91a9e62b8d2c
Harshit-7735 <letcs202205@global.org.in>
2024-07-24 12:37:39 +0530

    methods in Javascript

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,57 +1,104 @@
-/*
-const arr=[1,2,3,4,5];
-console.log(arr);
-arr.push(6);
-console.log(arr);
-arr.pop();
-console.log(arr);
-console.log(arr.length);
+const a = {
+  name: "Harshit",
+  age: 30,
+  isMarried: true,
 
-arr.unshift(0);
-console.log(arr);
-arr.shift();
-console.log(arr);
-*/
-/*
-const arr = ["apple", "banana", "cherry"];
-const callback = function (item, index) {
-  console.log(item, index);
+  greet: function () {
+    console.log("Hello");
+  },
 };
-arr.forEach(callback);
-*/
-
-
-/*
-const arr=["jpe","jane","jack"]
-arr.forEach(function(item,index){
-  console.log(item,index);
-})
-  */
-
-
-const arr=[1,2,3,4,5,6,7,8,9,10]
-const newArr=[]
-
-// for(let i=0;i<arr.length;i++){
-//   newArr.push(arr[i]*19)
+// for (const key in a) {
+//   console.log(key, a[key]);
 // }
 
-// arr.forEach(function(item){
-//   newArr.push(item*19)
-// })
+// console.log(Object.keys(a));
+// console.log(Object.values(a));
 
-// foreach doesn't return anything 
-// map returns
+// const b={}
+// const reducefunction =function(prev,curr){
+//   console.log(prev,curr);
+//   // Dry run of the code below
+//   // prev={}
+//   // curr=name
+//   // prev[name]=a[name]
+//   // prev={name:"Harshit"}
+//   // curr=age
+//   // prev[age]=a[age]
+//   // prev={name:"Harshit",age:30}
+//   // curr=isMarried
+//   // prev[isMarried]=a[isMarried]
+//   // prev={name:"Harshit",age:30,isMarried:true}
+//   // curr=greet
+//   // prev[greet]=a[greet]
+//   // prev={name:"Harshit",age:30,isMarried:true,greet:function(){console.log("Hello")}}
+//   prev[curr]=a[curr]
+//   return prev
+// }
+// Object.keys(a).reduce(reducefunction,b)
+// console.log(b);
+
+const arr = [
+  { name: "Harshit", age: 30 },
+  { name: "Amit", age: 25 },
+  { name: "Rahul", age: 35 },
+];
 /*
-const newArr2=arr.map(function(item){
-  return item*19;
-})
-console.log(newArr2);
+const b={}
+const reducefunction=function(prev,curr){
+  // dry run of the code below
+  // prev={}
+  // curr={name:"Harshit",age:30}
+  // prev[name]=age
+  // prev={Harshit:30}
+  // curr={name:"Amit",age:25}
+  // prev[Amit]=25
+  // prev={Harshit:30,Amit:25}
+  // curr={name:"Rahul",age:35}
+  // prev[Rahul]=35
+  // prev={Harshit:30,Amit:25,Rahul:35}
+  prev[curr.name]=curr.age // it will return the object with name as key and age as value 
+  return prev
+}
+console.log(arr.reduce(reducefunction,b));  
 */
 
-const reduceFunction=function(prev,curr){
-  console.log(prev,curr)
-  return prev+curr;
+/*
+const findFn = function (item, index) {
+  if (item.name === "Amit") {
+    return true;
+  }
+  return false;
+};
+const ele = arr.find(findFn);
+console.log(ele); // {name:"Amit",age:25}
+*/
+
+/*
+const filterfn=function(item,index){
+  if(item.age>30 && item.name==="Rahul"){
+    return true
+  }
+  return false
 }
-const res2=arr.reduce(reduceFunction,0)
-console.log(res2);
+const ele=arr.filter(filterfn)
+console.log(ele); // [{name:"Rahul",age:35}]
+*/
+
+/*
+const arrr=[1,2,3,4,5]
+const reduceFn=function(prev,curr){
+  return prev+curr
+}
+console.log(arrr.reduce(reduceFn,0)); // 15
+*/
+
+const arrr = [1, 2, 3, 4, 5];
+arrr.splice(1,0,99)
+arrr.splice(2,0,100)
+arrr.splice(3,0,101)
+console.log(arrr);
+
+console.log(arrr.splice(1,5)); // [99,100,101,2,3]
+console.log(arrr.splice(2,1)); // [100]
+console.log(arrr.splice(3,1)); // [101]
+console.log(arrr); // [1,2,3,4,5]
