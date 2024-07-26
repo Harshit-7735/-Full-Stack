0e7fd4ad9f5727dc7612fb572c5dfb6b81a2ccfb
Harshit-7735 <letcs202205@global.org.in>
2024-07-26 11:09:45 +0530

    DOM

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,12 +1,14 @@
-// ...REST Operator
-// The rest parameter syntax allows a function to accept an indefinite number of arguments as an array, providing a way to represent variadic functions in JavaScript.
-// The rest parameter syntax allows us to represent an indefinite number of arguments as an array.
-const sum = (...args) => {
-  console.log(args);
-};
-sum(1, 2, 3, 4, 5); // Output: [1, 2, 3, 4, 5]
+console.dir(document);
+console.log(document.body);
+console.dir(document.body);
 
-const sum1 = (...args) => {
-  return args.reduce((a, b) => a + b, 0);
-};
-console.log(sum1(1, 2, 3, 4, 5)); // Output: 15
+
+//Document object is the root node of the HTML document
+//It is the parent of all other nodes in the document
+//It is the entry point to the content of the document
+//It is the global object of the document
+//It is the owner of all other objects in the document
+//It is the interface to the content of the document
+
+
+// console.dir(document.body.children[0]);
