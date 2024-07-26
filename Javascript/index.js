ef9023a148e3d6a40f61f1ef10c7318aa0267674
Harshit-7735 <letcs202205@global.org.in>
2024-07-26 12:03:05 +0530

    DOM methods

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,14 +1,30 @@
-console.dir(document);
-console.log(document.body);
-console.dir(document.body);
+
+const node=document.getElementById('heading');
+/*
+// node.innerHTML='Hello World';
+node.innerHTML=`<i>this is italic</i>`
+node.style.color='red';
+console.log(node);
+console.dir(node);
+console.log(node.innerHTML);
+*/
+
+const listItems=document.getElementsByClassName('list-item');
+// get elements by class name return an array of matching elements  if no element found it will return an empty array
+// get elements by id return a single element 
+console.log(listItems);
+// console.log(node);
+
+// tag name is used to get elements by tag name 
+// it will return an array of matching elements
+const tag=document.getElementsByTagName('li');
+// console.log(tag);
+
+// query selector is used to get elements by css selector 
+// it will return the first matching element
+const element=document.querySelector('li:last-child');
+// console.log(element);
 
 
-//Document object is the root node of the HTML document
-//It is the parent of all other nodes in the document
-//It is the entry point to the content of the document
-//It is the global object of the document
-//It is the owner of all other objects in the document
-//It is the interface to the content of the document
-
-
-// console.dir(document.body.children[0]);
+const elements=document.querySelectorAll('li');
+console.log(elements);
