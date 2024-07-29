5f6aba31812af57be79a5b09fb5f1b1857f189c3
Harshit-7735 <letcs202205@global.org.in>
2024-07-29 13:36:45 +0530

    DOM  methods

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,30 +1,12 @@
+const heading = document.querySelector('h1');
+const body = document.querySelector('body');
+const content = document.querySelector('p');
+// heading.innerHTML = `<span style="color: red;">Hello</span>World!`;
 
-const node=document.getElementById('heading');
-/*
-// node.innerHTML='Hello World';
-node.innerHTML=`<i>this is italic</i>`
-node.style.color='red';
-console.log(node);
-console.dir(node);
-console.log(node.innerHTML);
-*/
-
-const listItems=document.getElementsByClassName('list-item');
-// get elements by class name return an array of matching elements  if no element found it will return an empty array
-// get elements by id return a single element 
-console.log(listItems);
-// console.log(node);
-
-// tag name is used to get elements by tag name 
-// it will return an array of matching elements
-const tag=document.getElementsByTagName('li');
-// console.log(tag);
-
-// query selector is used to get elements by css selector 
-// it will return the first matching element
-const element=document.querySelector('li:last-child');
-// console.log(element);
+// To add styles to the elements
+content.style =`
+background-color: #333;
+color: white;     
+`
 
 
-const elements=document.querySelectorAll('li');
-console.log(elements);
