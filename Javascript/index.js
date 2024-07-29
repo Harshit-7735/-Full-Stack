d2a3ab051280ebea762e45b5bdb63fddd0b5d89a
Harshit-7735 <letcs202205@global.org.in>
2024-07-29 14:13:15 +0530

    DOM METHODS AND API

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,12 +1,67 @@
-const heading = document.querySelector('h1');
-const body = document.querySelector('body');
-const content = document.querySelector('p');
-// heading.innerHTML = `<span style="color: red;">Hello</span>World!`;
+// SET Interval
 
-// To add styles to the elements
-content.style =`
-background-color: #333;
-color: white;     
-`
+const Heading = document.querySelector("h1");
+const body = document.querySelector("body");
+/*
+const HandleClick = () => {
+  // Function to be called
+  console.log("Hello World");
+};
+setInterval(HandleClick()); // Function to be called after every 1 second
+*/
+
+/*
+let count = 0;
+const log = () => {
+  Heading.innerHTML = count++;
+};
+setInterval(log, 1000); // Function to be called after every 1 second
+*/
+
+/*
+// Wap to generate random color after every 1 second
+setInterval(() => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    body.style=`
+    background-color: #${randomColor};`
+
+}, 1000);
+*/
+
+// Using Javascript and DOM api
+/*
+const todos=['Buy Milk','Go to Gym','Record Youtube Videos','Learn Javascript'];
+const li=document.createElement('li');
+console.log(li);
+// Adding text inside li element using innerHTML
+li.innerHTML='New Todo';
+
+// Using text node to add text inside li element 
+const textinsideli=document.createTextNode('New Todo');
+li.appendChild(textinsideli);
+console.log(textinsideli);
+*/
+
+
+/*
+const todos=['Buy Milk','Go to Gym','Record Youtube Videos','Learn Javascript'];
+const list=document.querySelector('ul');
+todos.forEach((todo)=>{
+    const li=document.createElement('li');
+    li.innerHTML=todo;
+    list.appendChild(li);
+
+})
+    */
+
+const input=document.querySelector('input');
+const list=document.querySelector('ul');
+
+const HandleClick=()=>{
+    const li=document.createElement('li');
+    li.innerHTML=input.value;
+    list.appendChild(li);
+    input.value='';
+}
 
 
