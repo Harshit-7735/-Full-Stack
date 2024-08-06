96f16fb3346e24b6f8fb02769346d7226bde74df
Harshit-7735 <letcs202205@global.org.in>
2024-08-06 12:23:07 +0530

    creation of promises

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,15 +1,10 @@
-const response = axios.get("https://jsonlaceholder.typicode.com/posts");
-console.log(response); // initially this will log a promise object and its state is pending
-// then is a promise function that is called when the promise is resolved or rejected
-// it takes a callback function as an argument
-// the callback function takes an argument that is the data returned from the promise
-response.then((data) => {
-  console.log(data);
-});
-
-
-// what if something goes wrong?
-// we can use the catch method to handle errors
-response.catch((error) => {
-  console.log(error,"data not found");
-});
+const fetchTodos =() => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // it takes 2 seconds to fetch the data from the server and return it as a promise object
+        // resolve([{id: 1, title: 'Todo 1'}, {id: 2, title: 'Todo 2'}]);
+        reject(new Error('Unable to fetch the data from the server'));
+    }, 2000);
+})
+fetchTodos()
+.then((data)=>{console.log(data)})
+.catch((error)=>{console.log(error)})
