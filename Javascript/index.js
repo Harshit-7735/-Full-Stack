21377b33dceb3108d54f458889110a836db58e7d
Harshit-7735 <letcs202205@global.org.in>
2024-08-06 11:23:46 +0530

    axios

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,32 +1,9 @@
-// console.log(1);
-// for(let i=0;i <10000;i++){
-// console.log('100p');
+// console.log(axios);
 
-// }
-// console.log(2);
-// console.log(3);
-/*
-const cb=()=>{
-    console.log("callback");
-}
-setTimeout(cb,5000);
-*/
-console.log(1);
+const response = axios.get('https://jsonplaceholder.typicode.com/todos');// it will take some time to get the response
+console.log(response);
 
-    setTimeout(() => {
-        console.log(4);
-    }, 0);
-setTimeout(()=>{
-    console.log(2);
-},0)
-
-for(let i=0;i<10000;i++){
-    console.log('100p');
-}
-console.log(3);
-
-// for loop executing first then settimeout function is executing because settimeout function is executing after the for loop is executed because of the event loop in javascript 
-// settimeout function is executing after the for loop is executed
-
-
-// set timeout is a native code Javascript doesn't know about the settime out function ..iot will wait in the queue and after all the functions is executed the set timeout will get executed
+setTimeout(() => {
+    console.log(response);
+}, 3000); // it will print the response after 3 seconds and it is fulfilled promise object with the response data and other properties 
+//it's because of the asynchronous nature of the axios.get() method and the setTimeout() function.
