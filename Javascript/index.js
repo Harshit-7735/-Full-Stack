fe5a12f7c56ed8470d372d7193faded048f3faaa
Harshit-7735 <letcs202205@global.org.in>
2024-08-06 11:42:14 +0530

    promises

--- Javascript/index.js
+++ Javascript/index.js
@@ -7,3 +7,13 @@ setTimeout(() => {
     console.log(response);
 }, 3000); // it will print the response after 3 seconds and it is fulfilled promise object with the response data and other properties 
 //it's because of the asynchronous nature of the axios.get() method and the setTimeout() function.
+
+
+
+//For better mechanism we will use promises
+// promises are the objects that represent the eventual completion or failure of an asynchronous operation and its resulting value.
+
+//promises have 3 states:
+//1. Pending: initial state, neither fulfilled nor rejected.
+//2. Fulfilled: meaning that the operation completed successfully.
+//3. Rejected: meaning that the operation failed.
