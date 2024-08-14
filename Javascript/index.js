56edeb57c79d7c6dc1c7c3fafb73b1802fe69b88
Harshit-7735 <letcs202205@global.org.in>
2024-08-14 12:24:31 +0530

    clipboard

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,9 @@
-console.log(navigator.geolocation.getCurrentPosition (function(position) {
-  console.log(position);
-}));
+// console.log(navigator.geolocation.getCurrentPosition (function(position) {
+//   console.log(position);
+// }));
+
+navigator.clipboard.writeText('Hello, World!').then(function() {
+  console.log('Copied to clipboard!');
+}, function() {
+  console.error('Unable to write to clipboard. :-(');
+});
