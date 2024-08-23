e28f46d488928c6d2090c88978b90c23131a40e4
Harshit-7735 <letcs202205@global.org.in>
2024-08-23 11:02:12 +0530

    Introduction to node js

---
+++ NodeJs/script.js
@@ -0,0 +1,9 @@
+//Node Js
+// Node Js is a server side platform which is used to build fast and scalable network applications.
+// It is an open source, cross platform runtime environment for developing server side and networking applications.
+// NVM (Node Version Manager) is a tool that allows you to download and install different versions of Node.js.
+// Node Js is written in C, C++, and JavaScript. 
+// Node Js is used to build back-end services like APIs, Microservices, and Web Applications.
+// Node Js is used to build front-end services like React, Angular, and Vue.
+// Node Js is used to build desktop applications like Electron.
+// A server is a computer program that provides services to other computer programs in the same or other computers.
