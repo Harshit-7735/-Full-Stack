ad642493efa94627b31dff3e7bab57d6c3b37a19
Harshit-7735 <letcs202205@global.org.in>
2024-08-23 11:26:31 +0530

    Some basics of node

--- NodeJs/script.js
+++ NodeJs/script.js
@@ -1,9 +1,21 @@
-//Node Js
-// Node Js is a server side platform which is used to build fast and scalable network applications.
-// It is an open source, cross platform runtime environment for developing server side and networking applications.
-// NVM (Node Version Manager) is a tool that allows you to download and install different versions of Node.js.
-// Node Js is written in C, C++, and JavaScript. 
-// Node Js is used to build back-end services like APIs, Microservices, and Web Applications.
-// Node Js is used to build front-end services like React, Angular, and Vue.
-// Node Js is used to build desktop applications like Electron.
-// A server is a computer program that provides services to other computer programs in the same or other computers.
+console.log('hi');
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+// it wont work because these are browser based functions and we are running this in nodejs environment
+// 
+// window.alert('hi');
+// console.log(window);
+// console.log(document);
+// console.log(localStorage);
+
+
+
+//promises will work in nodejs
+//fetch will not work in nodejs
+//async await will work in nodejs
+setTimeout(() => {  
+    console.log('setTimeout');
+}, 3000);
+    
+
