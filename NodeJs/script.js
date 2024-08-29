39d55597851be5f49241250cd4e9e732bb75406d
Harshit-7735 <letcs202205@global.org.in>
2024-08-29 11:59:12 +0530

    Fs module

--- NodeJs/script.js
+++ NodeJs/script.js
@@ -1,3 +1,10 @@
-const path = require('path')
-console.log(path.resolve())
-console.log(__dirname);
+const fs = require('fs');
+
+const data = "Hello World"
+
+fs.writeFileSync('test.txt',data)
+fs.writeFile('test2.txt',data,(err)=>{
+    if(err) throw err;
+    console.log("File written successfully");
+    
+})
