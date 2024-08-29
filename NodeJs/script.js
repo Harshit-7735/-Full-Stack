1ed6f8244c49e3f3c1e87ea4415efbce1e44a542
Harshit-7735 <letcs202205@global.org.in>
2024-08-29 12:20:27 +0530

    fs.remove file

--- NodeJs/script.js
+++ NodeJs/script.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("test.txt", "utf8");
-fs.readFile('test.txt','utf8',(err,data)=>{
+const data= fs.rm('test.txt',(err)=>{
     if (err) throw err;
-    console.log(data,'async')
+    console.log(data,'file removed');
 })
-console.log(data,'sync');
