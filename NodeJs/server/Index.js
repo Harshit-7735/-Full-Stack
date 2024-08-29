302d6c9856aeb5c7ac558870f9417696d832b5d3
Harshit-7735 <letcs202205@global.org.in>
2024-08-29 13:51:53 +0530

    Scripts dependencies and Dev dependencies

--- NodeJs/server/Index.js
+++ NodeJs/server/Index.js
@@ -1,4 +1,17 @@
-const { v4: uuidv4 } = require('uuid');
-const id = uuidv4();
-console.log(id);
+// const { v4: uuidv4 } = require('uuid');
+// const id = uuidv4();
+// console.log(id);
 
+const axios= require('axios')
+
+const fetchdata= async()=>{
+    try {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        console.log(res.data);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+fetchdata()
