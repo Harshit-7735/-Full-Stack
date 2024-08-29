f6d434277c4ee4ac3bae331a98b23e4ce2a50d7f
Harshit-7735 <letcs202205@global.org.in>
2024-08-29 11:51:43 +0530

    Path Module

--- NodeJs/script.js
+++ NodeJs/script.js
@@ -1,11 +1,3 @@
-// console.log(module)
-// console.log(require('./Math.js'))
-
-// console.log(Math.add(1,2,3,4,5))
-// console.log(require('./Math.js').add(1,2,3,4,5))
-
-const {add,subtract,multiply,divide} = require('./Math.js')
-console.log(add(1,2,3,4,5))
-console.log(subtract(1,2,3,4,5))
-console.log(multiply(1,2,3,4,5))
-console.log(divide(1,2,3,4,5))
+const path = require('path')
+console.log(path.resolve())
+console.log(__dirname);
