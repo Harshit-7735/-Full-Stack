8385bfd772368bdcf0a72b1f9852441d6991780c
Harshit-7735 <letcs202205@global.org.in>
2024-08-29 12:30:15 +0530

    fs.remove file and create file

--- NodeJs/script.js
+++ NodeJs/script.js
@@ -1,6 +1,19 @@
 const fs = require("fs");
 
-const data= fs.rm('test.txt',(err)=>{
-    if (err) throw err;
-    console.log(data,'file removed');
-})
+// const data= fs.rm('test.txt',(err)=>{
+//     if (err) throw err;
+//     console.log(data,'file removed');
+// })
+
+const createFile = (data,name)=>{
+    try {
+         fs.writeFileSync(name,data)
+        console.log('file written');
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+createFile('hello world','test.txt')
