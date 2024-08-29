ca30fa77f9cbf951951e5a1c7bf038b15db5e989
Harshit-7735 <letcs202205@global.org.in>
2024-08-29 11:39:24 +0530

    Export and import Module

--- NodeJs/script.js
+++ NodeJs/script.js
@@ -1,21 +1,11 @@
-console.log('hi');
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
-// it wont work because these are browser based functions and we are running this in nodejs environment
-// 
-// window.alert('hi');
-// console.log(window);
-// console.log(document);
-// console.log(localStorage);
+// console.log(module)
+// console.log(require('./Math.js'))
 
+// console.log(Math.add(1,2,3,4,5))
+// console.log(require('./Math.js').add(1,2,3,4,5))
 
-
-//promises will work in nodejs
-//fetch will not work in nodejs
-//async await will work in nodejs
-setTimeout(() => {  
-    console.log('setTimeout');
-}, 3000);
-    
-
+const {add,subtract,multiply,divide} = require('./Math.js')
+console.log(add(1,2,3,4,5))
+console.log(subtract(1,2,3,4,5))
+console.log(multiply(1,2,3,4,5))
+console.log(divide(1,2,3,4,5))
