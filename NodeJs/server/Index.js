14fbde2fae23576efa8bb61cbf15b947eefe06f7
Harshit-7735 <letcs202205@global.org.in>
2024-09-25 13:23:39 +0530

    express server

--- NodeJs/server/Index.js
+++ NodeJs/server/Index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require("fs/promises")
+const fs = require("fs/promises");
 // console.log(express);
 
 const PORT = 8080;
@@ -23,18 +23,18 @@ app.get("/", (req, res) => {
 //     res.status(200).send('todos')
 // })
 
-app.get('/todos',async(req,res)=>{
-    try {
-        const todos = await fs.readFile('./db.json','utf-8')
-        console.log(todos)
-        res.status(200).send(todos)
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
-        
-        
-    }
-})
+app.get("/todos", async (req, res) => {
+  try {
+    const {count} = req.query
+    const todos = await fs.readFile("./db.json", "utf-8");
+    const parsedtodos= JSON.parse(todos)
+    console.log(todos);
+    res.status(200).json((parsedtodos.slice(0,count)));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
