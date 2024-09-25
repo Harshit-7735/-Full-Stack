3721a1fa5bb81cbaa6ed6ecc9c43c7f2dde88a64
Harshit-7735 <letcs202205@global.org.in>
2024-09-25 12:12:00 +0530

    req and res

--- NodeJs/server/Index.js
+++ NodeJs/server/Index.js
@@ -2,8 +2,22 @@ const express = require('express')
 // console.log(express);
 
 const PORT=8080;
+// express instance
 const app = express()
 
+app.get('/',(req,res)=>{
+    // console.log(req);
+    const {headers,method,url,path,query}=req
+    console.log({
+        headers,
+        method,
+        url,
+        path,query
+    });
+    
+    res.send('hello world')
+    
+})
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     
