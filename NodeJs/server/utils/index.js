285937b12a63748664ec6207f8a9edcd4228ca74
Harshit-7735 <letcs202205@global.org.in>
2024-10-09 15:55:50 +0530

    api endpoints

---
+++ NodeJs/server/utils/index.js
@@ -0,0 +1,18 @@
+// it will add data to the database  and return true if data is added successfully otherwise false if there is an error while adding data to the database.
+
+const fs = require("fs/promises");
+
+const addToDb = async (data, path) => {
+    try {
+        const dbData = await fs.readFile(path, "utf8");
+        const parsedData = JSON.parse(dbData);
+        parsedData.push(data);
+        await fs.writeFile(path, JSON.stringify(parsedData));
+        return true;
+    } catch (error) {
+        console.log(error)
+        return false;
+    }
+}
+
+module.exports.addToDb = addToDb;
